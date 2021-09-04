1f85d4677b794ae5aaf295a983861ff2a057442a
Егор <egornesvickij@gmail.com>
2021-09-04 11:59:20 +0500

    added container

---
+++ src/components/container/container.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './container.css'
+
+export const Container = (props) => {
+  return (
+    <div className="container">
+      {props.children}
+    </div>
+  )
+}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,22 @@
-import './header.css'
+import './header.css';
+import { Container } from '../container/container';
 
 export const Header = () => {
 
     return (
         <div className="header">
-            <div className="header__nav">
-                <ul className="header__nav-list">
-                    <li className="header__nav-item active">
-                        <a href="">Проверка файлов</a>
-                    </li>
-                    <li className="header__nav-item">
-                        <a href="">Хранилище</a>
-                    </li>
-                </ul>
-            </div>
+            <Container>
+                <div className="header__nav">
+                    <ul className="header__nav-list">
+                        <li className="header__nav-item active">
+                            <a href="">Проверка файлов</a>
+                        </li>
+                        <li className="header__nav-item">
+                            <a href="">Хранилище</a>
+                        </li>
+                    </ul>
+                </div>
+            </Container>
         </div>
     )
 }
