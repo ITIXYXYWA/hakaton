fb6d086999cad6df9f2a3d02d3fba74eafd29286
ITIXYXYWA <ukv220@gmail.com>
2021-09-04 11:31:54 +0500

    add routers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'normalize.css';
+import { BrowserRouter } from 'react-router-dom';
+// import 'normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
