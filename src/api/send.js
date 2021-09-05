f067abb04a292779bd2f5757df2208db5685528a
DmitryqV <vlasenko_dima@icloud.com>
2021-09-05 09:33:49 +0500

    added checks

--- src/api/send.js
+++ src/api/send.js
@@ -1,5 +1,5 @@
 import { CONVERT } from "./urls";
-
+import { getText } from './get-text';
 export const getFormData = (file) => {
 
     const formData = new FormData();
@@ -10,30 +10,30 @@ export const getFormData = (file) => {
 export const getConverData = (file) => {
     const reader = new FileReader();
     reader.onloadend = () => {
-      // use a regex to remove data url part
-      const base64String = reader.result
-        .replace("data:", "")
-        .replace(/^.+,/, "");
+        const base64String = reader.result
+            .replace("data:", "")
+            .replace(/^.+,/, "");
 
-        fetch(CONVERT, {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({
-            Parameters: [
-                {
-                    Name: "File",
-                    FileValue: {
-                        Name: file[0].name,
-                        Data: base64String
+            fetch(CONVERT, {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({
+                Parameters: [
+                    {
+                        Name: "File",
+                        FileValue: {
+                            Name: file[0].name,
+                            Data: base64String
+                        }
+                    },
+                    {
+                        Name: "StoreFile",
+                        Value: true
                     }
-               },
-                {
-                    Name: "StoreFile",
-                    Value: true
-                }
-            ]
-        })
-    }).then(response => response.json()).then(res => console.log(res));
+                ]
+            })
+        }).then(response => response.json())
+        .then(res => getText(res.Files[0].Url));
     };
     reader.readAsDataURL(new Blob([file[0]], {type: "application/octet-stream"}));
 
