532557b92ec81af0e4ae883624c6abe3b134c7b3
ITIXYXYWA <ukv220@gmail.com>
2021-09-05 11:53:08 +0500

    fixes: end

--- src/api/urls.js
+++ src/api/urls.js
@@ -2,4 +2,4 @@ const URL = 'http://10.3.1.11:3000'
 
 export const URL_SEND_DATA = `${URL}/submit`
 
-export const CONVERT = 'https://v2.convertapi.com/convert/pdf/to/txt?Secret=oO8gfpbqeU4Iw6z3'
+export const CONVERT = 'https://v2.convertapi.com/convert/pdf/to/txt?Secret=LQCjxI9fVmMBK8VS'
